d4819d06640bcee2c99d158ea3ae9ebf780a44bd
magdalena <magdalena>
2017-01-28 20:27:41 +0100

    adding properties stub

---
+++ src/test/java/mlz/money/CalculatorProperties.java
@@ -0,0 +1,19 @@
+package mlz.money;
+
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.When;
+import com.pholser.junit.quickcheck.generator.InRange;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import org.junit.runner.RunWith;
+import static org.hamcrest.Matchers.greaterThan;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.*;
+import static org.junit.Assume.assumeThat;
+
+
+public class CalculatorProperties {
+
+
+}
