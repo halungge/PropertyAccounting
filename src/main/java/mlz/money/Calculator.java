b3426ced93ffc0492b7fa8968254e288cade39d3
magdalena <magdalena>
2017-01-28 19:48:07 +0100

    adding Calculator Stub and failing example Tests

---
+++ src/main/java/mlz/money/Calculator.java
@@ -0,0 +1,12 @@
+package mlz.money;
+
+
+import java.math.BigDecimal;
+
+public class Calculator {
+    public static BigDecimal add(BigDecimal amount1, BigDecimal amount2) {
+        return BigDecimal.ZERO;
+
+    }
+
+}
---
+++ src/test/java/mlz/money/CalculatorTest.java
@@ -0,0 +1,31 @@
+package mlz.money;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.*;
+
+
+public class CalculatorTest {
+    @Test
+    public void addAmounts(){
+        BigDecimal amount1 = new BigDecimal("2.3");
+        BigDecimal amount2 = new BigDecimal("2.0");
+        assertTrue(new BigDecimal("4.3").compareTo(Calculator.add(amount1, amount2)) == 0);
+    }
+
+
+    @Test
+    public void addZeroDoesNotChangeValue(){
+        BigDecimal amount1 = new BigDecimal("2.3");
+        BigDecimal amount2 = new BigDecimal("0.0");
+        assertTrue(amount1.compareTo(Calculator.add(amount1, amount2)) == 0);
+    }
+
+    // if we fix these two cases, who guarantees that it will work for all
+    // others as well?
+
+
+}
