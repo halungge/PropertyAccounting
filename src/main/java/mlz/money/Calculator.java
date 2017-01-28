8945fcb43acbb1a0a8a6619ced1891e352536f9d
magdalena <magdalena>
2017-01-28 20:04:11 +0100

    add implementation to fix example tests

--- src/main/java/mlz/money/Calculator.java
+++ src/main/java/mlz/money/Calculator.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 
 public class Calculator {
     public static BigDecimal add(BigDecimal amount1, BigDecimal amount2) {
-        return BigDecimal.ZERO;
+        return amount1.add(amount2);
 
     }
 
